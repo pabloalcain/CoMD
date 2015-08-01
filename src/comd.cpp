1ead39bda9951680277e217b7ae546bc6ff9f56f
Pablo Alcain <pabloalcain@gmail.com>
2015-08-01 18:49:33 -0300

    Added nsteps in argv

--- src/comd.cpp
+++ src/comd.cpp
@@ -1,12 +1,18 @@
 #include "comd.h"
 
 int main(int argc, char* argv[]) {
+    int nsteps;
     Box box = Box(5.0);
     Particles particles = Particles(500, 500, box);
     Potential potential = Potential();
     Integrator integrator = Integrator(0.01);
-
+    
+    if (argc != 2) {
+	std::cerr << "usage: " << argv[0] << " nsteps" << std::endl;
+	exit(1);
+    }
+    nsteps = atoi(argv[1]);
     std::cout << "Initializing system" << std::endl;
     System sys = System(&box, &particles, &potential, &integrator);
-    sys.run(1000);
+    sys.run(nsteps);
 }
