5c30db49b2434b1f559fc761fc8798109c898495
leandro <leandro@ara>
2015-08-02 17:22:24 -0300

    Added dump files hehe

---
+++ src/dump.cpp
@@ -0,0 +1,40 @@
+#include "dump.h"
+
+Dump::Dump(int _nfreq, std::ofstream *fname) {
+  dumpfile = fname;
+  nfreq = _nfreq;
+}
+
+Dump::~Dump() {
+  dumpfile->close();
+}
+
+void Dump::write(int step, Particles *part, Box *box) {
+  write_header(step, part, box);
+  write_part(part);
+}
+
+void Dump::write_header(int step, Particles *part, Box *box) {
+  *dumpfile << "ITEM: TIMESTEP" << std::endl;
+  *dumpfile << step << std::endl;
+  *dumpfile << "ITEM: NUMBER OF ATOMS" << std::endl;
+  *dumpfile << part->N << std::endl;
+  *dumpfile << "ITEM: BOX BOUNDS p p p" << std::endl;
+  *dumpfile << 0 << " " << box->size[0] << std::endl;
+  *dumpfile << 0 << " " << box->size[1] << std::endl;
+  *dumpfile << 0 << " " << box->size[2] << std::endl;
+  *dumpfile << "ITEM: ATOMS id type x y z vx vy vz" << std::endl;
+}
+
+void Dump::write_part(Particles *part) {
+  for (int i = 0; i < part->N; i++) {
+    int is = part->isospin[i]?1:0;
+    *dumpfile << i;
+    *dumpfile << " " << is;
+    for (int l = 0; l < 3; l++) 
+      *dumpfile << " " << part->x[3*i + l]; 
+    for (int l = 0; l < 3; l++) 
+      *dumpfile << " " << part->v[3*i + l];
+    *dumpfile << std::endl;
+  }
+}
---
+++ src/dump.h
@@ -0,0 +1,26 @@
+#ifndef DUMP_H
+#define DUMP_H
+
+#include <fstream>
+#include <string>
+
+#include "particles.h"
+#include "box.h"
+
+class Dump
+{
+/* Dump class, with the write() method. Prepared to mimic lammps
+   style with id, type, x, y, z, vx, vy, vz */
+
+  std::ofstream *dumpfile;
+  void write_header(int step, Particles *part, Box *box);
+  void write_part(Particles *part);
+  
+ public:
+  int nfreq; /* How often we write the dump */
+  Dump(int nfreq, std::ofstream *fname);
+  ~Dump();
+  void write(int step, Particles *part, Box *box);
+};
+
+#endif
