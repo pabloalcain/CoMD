90c8d76119f9314f264c857a900da3a144d37f37
leandroriverogonzalez <leandroriverogonzalez@gmail.com>
2016-04-26 21:23:49 -0300

    Added files via upload
    Update phi and phsi, I not sure its right.

---
+++ potential.cpp
@@ -0,0 +1,71 @@
+#include "potential.h"
+
+Potential::Potential(Particles *part) {
+  /* The default constructor has 2 types of particles and sets the
+   values as in CoMD. We pass the particles to also know the size in
+   q-space. */
+
+  r0 = ;
+  p0 = ;
+  v0 = ;
+  hbarra = ;
+  D = ;
+  
+  
+  int n = part->ntypes;
+
+  t0 = -356.0;
+  t3 = 303.0;
+  r0 = 0.16;
+  asym = 32.0;
+  cs = -0.33;
+  e = 1.2;
+  sigma_r = part->sigma_r;
+  u = 7.0/6.0;
+
+  rcut = (double **) malloc((n+1)*sizeof(double *));
+  for (int i = 0; i < n+1; i++) {
+    rcut[i] = (double *) malloc((n+1)*sizeof(double));
+  }
+
+  phicut = (double **) malloc((n+1)*sizeof(double *));
+  for (int i = 0; i < n+1; i++) {
+    phicut[i] = (double *) malloc((n+1)*sizeof(double));
+    
+  }
+
+  rcut[1][1] = 5.0;
+  rcut[2][1] = 5.0;
+  rcut[1][2] = 5.0;
+  rcut[2][2] = 5.0;
+
+  phicut[1][1] = 0.0;
+  phicut[2][1] = 0.0;
+  phicut[1][2] = 0.0;
+  phicut[2][2] = 0.0;
+
+  
+  dphi(rcut[1][1], 1, 1, &(phicut[1][1]));
+  dphi(rcut[2][1], 2, 1, &(phicut[2][1]));
+  dphi(rcut[1][2], 1, 2, &(phicut[1][2]));
+  dphi(rcut[2][2], 2, 2, &(phicut[2][2]));
+}
+
+
+double Potential::dphi(double r, int t1, int t2, double *pe) {
+
+  return -(r/(r0^2))*v0*((hbarra/(r0*p0))^D)*pow(e,(-p*p/(2*p0^2)))*pow(e,(-r*r/(2*r0^2)));
+}
+
+double Potential::dpsi(double r, int t1, int t2, double *pe) {
+
+  return -(p/(p0^2))*v0*((hbarra/(r0*p0))^D)*pow(e,(-p*p/(2*p0^2)))*pow(e,(-r*r/(2*r0^2)));
+}
+
+
+
+
+    
+    
+    
+    
