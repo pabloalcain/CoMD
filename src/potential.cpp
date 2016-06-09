b59f8334ebbe3f0af2198ac1fda55184c7e22b8c
Leandro Rivero <lrivero@localhost.localdomain>
2016-06-08 21:33:27 -0300

    potential.cpp

--- src/potential.cpp
+++ src/potential.cpp
@@ -1,17 +1,18 @@
 #include "potential.h"
+#include <iostream>
 
-Potential::Potential(Particles *part) {
+Potential::Potential() {
   /* The default constructor has 2 types of particles and sets the
    values as in CoMD. We pass the particles to also know the size in
    q-space. */
 
-  r0 = ;
-  p0 = ;
-  v0 = ;
-  hbarra = ;
-  D = ;
+  r0 = 1;
+  p0 = 1;
+  v0 = 1;
+  hbarra = 1;
+  D = 1;
   
-  
+  /*
   int n = part->ntypes;
 
 
@@ -44,20 +45,21 @@ Potential::Potential(Particles *part) {
   dphi(rcut[2][1], 2, 1, &(phicut[2][1]));
   dphi(rcut[1][2], 1, 2, &(phicut[1][2]));
   dphi(rcut[2][2], 2, 2, &(phicut[2][2]));
+  */
 }
 
 
-double Potential::dphi(double r, double p, int t1, int t2, double *pe) {
-
-  return -(r/(r0^2))*v0*((hbarra/(r0*p0))^D)*pow(e,(-p*p/(2*p0^2)))*pow(e,(-r*r/(2*r0^2)));
+double Potential::dphi(double dr, double dp, double *pe) {
+  double num = -(dr/ (pow(r0, 2)) ) * v0 * pow( (hbarra/(r0 * p0)), D);
+  double exp = pow( e, -dp * dp / (2 * pow(p0, 2)) ) * pow( e, -dr * dr / (2 * pow(r0, 2)) );
+  return num * exp;
 }
 
-double Potential::dpsi(double r, double p, int t1, int t2, double *pe) {
-
-  return -(p/(p0^2))*v0*((hbarra/(r0*p0))^D)*pow(e,(-p*p/(2*p0^2)))*pow(e,(-r*r/(2*r0^2)));
+double Potential::dpsi(double dr, double dp, double *pe) {
+  double num = -(dp/ (pow(p0, 2)) ) * v0 * pow( (hbarra/(r0 * p0)), D);
+  double exp = pow( e, -dp * dp / (2 * pow(p0, 2)) ) * pow( e, -dr * dr / (2 * pow(r0, 2)) );
+  return num * exp;
 }
-
-
 
 
     
